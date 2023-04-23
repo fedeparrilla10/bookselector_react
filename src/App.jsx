34f56c19384c55d070ca37ffb607a50c5c9840cd
fedeparrilla10 <federicoivanpd@gmail.com>
2023-04-23 20:30:20 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,30 @@ import RandomBook from "./components/RandomBook";
 
 const App = () => {
   const [book, setBook] = useState("");
+  const [fantasy, setFantasy] = useState(false);
+  const [thriller, setThriller] = useState(false);
+  const [fiction, setFiction] = useState(false);
+  const [scifi, setScifi] = useState(false);
 
   const getBooks = () => {
-    let randomBook = Math.floor(Math.random() * books.length);
-    setBook(books[randomBook]);
+    let filteredBooks = books.filter((book) => {
+      if (
+        (fantasy && book.genre === "Fantasía") ||
+        (thriller && book.genre === "Thriller") ||
+        (fiction && book.genre === "Ficción") ||
+        (scifi && book.genre === "Ciencia Ficción")
+      ) {
+        return true;
+      }
+      return false;
+    });
+
+    if (!fantasy && !thriller && !fiction && !scifi) {
+      filteredBooks = books;
+    }
+
+    let randomBook = Math.floor(Math.random() * filteredBooks.length);
+    setBook(filteredBooks[randomBook]);
   };
 
   return (
@@ -21,6 +41,50 @@ const App = () => {
       ) : (
         <div className="mainpage-container">
           <MainPage />
+
+          <form>
+            <div className="fantasy-books">
+              <label htmlFor="fantasy">Fantasía</label>
+              <input
+                type="checkbox"
+                name="fantasy"
+                id="fantasy"
+                checked={fantasy}
+                onChange={() => setFantasy(!fantasy)}
+              />
+            </div>
+
+            <div className="thriller-books">
+              <label htmlFor="thriller">Thriller</label>
+              <input
+                type="checkbox"
+                name="thriller"
+                id="thriller"
+                checked={thriller}
+                onChange={() => setThriller(!thriller)}
+              />
+            </div>
+
+            <div className="fiction-books">
+              <label htmlFor="fiction">Ficción</label>
+              <input
+                type="checkbox"
+                name="fiction"
+                id="fiction"
+                checked={fiction}
+                onChange={() => setFiction(!fiction)}
+              />
+            </div>
+
+            <label htmlFor="scifi">Ciencia Ficción</label>
+            <input
+              type="checkbox"
+              name="scifi"
+              id="scifi"
+              checked={scifi}
+              onChange={() => setScifi(!scifi)}
+            />
+          </form>
           <button onClick={getBooks}>¡Recomiéndame un libro!</button>
         </div>
       )}
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,22 +1,22 @@
 const MainPage = () => {
   return (
-    <div>
+    <>
       <img src="img/library.png" alt="Book" />
       <h1>BookSelector</h1>
       <h2>¿Terminaste tu libro y no sabes con cuál seguir?</h2>
-      <p>
+      <p className="first-description">
         BookSelector es una aplicación web diseñada para todos aquellos lectores
         empedernidos que siempre que terminan un libro sufren para elegir el
         siguiente. Aquellos que sufran para tomar decisiones: esta página esta
         dedicada a ustedes.
       </p>
-      <p>
+      <p className="second-description">
         En cada uno de los libros agregamos un link a su correspondiente página
         en GoodReads, para que puedas entrar y ver las diferentes reseñas de los
         usuarios. Cada obra va acompañada también por su cantidad de páginas y
         su género, para que veas si es un buen match para vos.
       </p>
-    </div>
+    </>
   );
 };
 
